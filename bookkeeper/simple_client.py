3d61f7b249f556992d9f30ed22df4de01f57721a
LilVan <47319039+LilVan@users.noreply.github.com>
2023-03-13 22:20:51 +0300

    simple_client recovered

--- bookkeeper/simple_client.py
+++ bookkeeper/simple_client.py
@@ -5,12 +5,10 @@
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
 from bookkeeper.repository.memory_repository import MemoryRepository
-from bookkeeper.repository.sqlite_repository import SQLiteRepository
 from bookkeeper.utils import read_tree
 
-
-cat_repo = SQLiteRepository[Category]('test.db', Category)
-exp_repo = SQLiteRepository[Expense]('test.db', Expense)
+cat_repo = MemoryRepository[Category]()
+exp_repo = MemoryRepository[Expense]()
 
 cats = '''
 продукты
@@ -22,7 +20,7 @@ cats = '''
 одежда
 '''.splitlines()
 
-#Category.create_from_tree(read_tree(cats), cat_repo)  # TODO: не выполнять при каждом запуске
+Category.create_from_tree(read_tree(cats), cat_repo)
 
 while True:
     try:
@@ -31,28 +29,17 @@ while True:
         break
     if not cmd:
         continue
-
-    #if cmd == '2':
-     #   print(*cat_repo.get(2), sep='\n')
-
     if cmd == 'категории':
         print(*cat_repo.get_all(), sep='\n')
     elif cmd == 'расходы':
-        print(exp_repo.get(3), sep='\n')
-    elif cmd == 'get':
-        print(cat_repo.get(3), sep='\n')
-    elif cmd == 'del':
-        cat_repo.delete(3)
-    elif cmd == 'upd':
-        obj2 = Category('Meat', 2)
-        obj2.pk = 2
-        cat_repo.update(obj2)
-    elif cmd == 'cond':
+        print(*exp_repo.get_all(), sep='\n')
+    elif cmd[0].isdecimal():
+        amount, name = cmd.split(maxsplit=1)
         try:
-            print(cat_repo.get_all({'name': 'продукты', 'id': 1}))
+            cat = cat_repo.get_all({'name': name})[0]
         except IndexError:
-            print(f'категория продукты не найдена')
+            print(f'категория {name} не найдена')
             continue
-#        exp = Expense(int(amount), cat.pk)
-#        exp_repo.add(exp)
-#        print(exp)
+        exp = Expense(int(amount), cat.pk)
+        exp_repo.add(exp)
+        print(exp)
